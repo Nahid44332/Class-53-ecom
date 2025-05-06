b2c77ef53af510f5ed64c9e8efb9b84a7e2f1f86
Nahid44332 <nahidhossen3278@gmail.com>
2025-05-06 11:51:27 +0600

    E-Commerce

---
+++ example-app/resources/views/frontend/index.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organic Pure Vegetable</title>
+    <link rel="stylesheet" href="frontend/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header class="header-section">
+        <div>
+            <h1 class="hero">Organic Pure
+                Vegetable</h1>
+            <p class="hero-1">Be fit your health. Enjoy the flavor of freshly picked product</p>
+            <a href="#Checkout" class="btn button">Buy Now</a>
+        </div>
+    </header>
+    <!-- product Descriptions -->
+     <section>
+        <div class="container mt-5 why">
+            <h2>Why Choose Our Product</h2>
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                    <img src="frontend/img/1.jpeg" height="190" width="210">
+                    <h5>Feature-1</h5>
+                    <p>This is a premium product</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                    <img src="frontend/img/1.jpeg" height="190" width="210">
+                    <h5>Feature-2</h5>
+                    <p>This is a premium product</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                    <img src="frontend/img/2.jpeg" height="190" width="210">
+                    <h5>Feature-3</h5>
+                    <p>This is a premium product</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                <img src="frontend/img/3.jpeg" height="190" width="210">
+                <h5>Feature-4</h5>
+                <p>This is a premium product</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                    <img src="frontend/img/4.jpeg" height="190" width="210">
+                    <h5>Feature-5</h5>
+                    <p>This is a premium product</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="Product-card">
+                    <img src="frontend/img/5.jpeg" height="190" width="210">
+                    <h5>Feature-6</h5>
+                    <p>This is a premium product</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+     </section>
+     <!-- customer Review -->
+      <section>
+        <div class="container mt-5 why">
+            <h2>What our Customer Say</h2>
+            <div class="row">
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div class="Product-card">
+                        <img src="frontend/img/7.jpeg" height="190" width="210">
+                        <h5>Adul Vai</h5>
+                        <p>This Organic Vegetable Is Very Healthfull and Frash Mind</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div class="Product-card">
+                        <img src="frontend/img/8.jpeg" height="190" width="210">
+                        <h5>Kuddus Nana</h5>
+                        <p>This product Is Best Organic Vegetable in al of country</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div class="Product-card">
+                        <img src="frontend/img/9.jpeg" height="190" width="210">
+                        <h5>Bilkish</h5>
+                        <p>It's Organic Vegetable Product our Country's Frash Product</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div  class="Product-card">
+                        <img src="frontend/img/10.jpeg" height="190" width="210">
+                        <h5>Sokina Akter</h5>
+                        <p>It's Organic Vegetable Product our Country's Frash Product</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div  class="Product-card">
+                        <img src="frontend/img/11.jpeg" height="190" width="210">
+                        <h5>Sefali Begum</h5>
+                        <p>It's Organic Vegetable Product our Country's Frash Product</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+      <!-- cheakout -->
+       <section class="cheakout-section">
+            <div class="container mt-5 cheakout" id="Checkout">
+                <h2>Checkout</h2>
+                <form action="" method="" class="cheakout-form">
+                    <div class="mt-3">
+                        <label for="" class="form-label">Full Name*</label>
+                        <input type="text" class="form-control" name="" value="" id="" placeholder="Enter Full Name" required>
+                    </div>
+                    <div class="mt-3">
+                        <label for="" class="form-label">Phone Number*</label>
+                        <input type="text" class="form-control" name="" value="" id="" placeholder="Enter Phone Number" required>
+                    </div>
+                    <div class="mt-3">
+                        <label for="" class="form-label">Address*</label>
+                        <textarea class="form-control" name="" id="" placeholder="Enter Full Address"></textarea>
+                    </div>
+                    <div class="mt-3">
+                        <label for="" class="form-label">Area*</label>
+                        <select class="form-control" name="" id="">
+                            <option value="" selected disabled>Select Area</option>
+                            <option value="In-Dhaka">Inside Dhaka (80)</option>
+                            <option value="Out-Dhaka">Outside Dhaka (150)</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn button-1 mt-2">Confirm Order</button>
+                </form>
+            </div>
+       </section>
+       <!-- footer -->
+        <footer class="footer">
+            <div class="mt-5 container">
+                <div class="footer-contain">
+                    <div class="footer-logo">
+                        <img src="https://placehold.co/150x60" alt="">
+                    </div>
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook icon"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube icon-2"></i></a>
+                        <a href="#"><i class="fa-brands fa-x-twitter icon-3"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin icon-4"></i></a>
+                    </div>
+                    <p>&copy 2024 Landingpage. All Right Reserves</p>
+                </div>
+            </div>
+        </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
